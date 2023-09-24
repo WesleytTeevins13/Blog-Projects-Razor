68eb78ec343a8c1191b2ddbc2c51a3296b2fc151
GenesisCoding <sniperrr.teevins@gmail.com>
2023-09-24 14:55:38 -0400

    Blog Files
    All files as of right now but still lots of work to do.

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+// Filter Js
+$(document).ready(function () {
+    $('.filter-item').click(function () {
+        const value = $(this).attr('data-filter')
+        if (value == 'all') {
+            $('po-box').show('1000')
+        }
+        else {
+            $('.post-box').not('.' + value).hide('1000')
+            $('.post-box').filter('.' + value).show('1000')
+        }
+    })
+})
+
